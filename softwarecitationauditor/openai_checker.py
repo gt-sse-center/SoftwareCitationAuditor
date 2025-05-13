0fc683227b55eb0014eecb7e51b6de675529a9a6
Ketan Bhardwaj <ketanbj@cc.gatech.edu>
2025-05-13 15:46:27 -0400

    some functional fixes

--- softwarecitationauditor/openai_checker.py
+++ softwarecitationauditor/openai_checker.py
@@ -15,8 +15,8 @@ def extract_and_check_software(body_text, bibliography_text, pdf_filename, provi
     report_name = os.path.join("reports", f"{base_name}_report.md")
 
     prompt_template = load_prompt_template()
-    prompt = prompt_template.replace("{{BODY_TEXT}}", body_text[:4000]) \
-                            .replace("{{BIBLIOGRAPHY_TEXT}}", bibliography_text[:4000])
+    prompt = prompt_template.replace("{{BODY_TEXT}}", body_text) \
+                            .replace("{{BIBLIOGRAPHY_TEXT}}", bibliography_text)
 
     answer = ""
     if provider == "openai":
